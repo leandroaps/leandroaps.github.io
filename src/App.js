d7931aa8dec719468ddd51298140e7a238bbb61d
Leandro A. Siqueira <leandroaps@gmail.com>
2018-07-25 16:55:08 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,42 +1,30 @@
-import React, { Component } from 'react';
-import ReactGA from 'react-ga';
-import $ from 'jquery';
-import './App.css';
-import Header from './Components/Header';
-import Footer from './Components/Footer';
-import About from './Components/About';
-import Resume from './Components/Resume';
-import Contact from './Components/Contact';
-import Testimonials from './Components/Testimonials';
-import Portfolio from './Components/Portfolio';
+import React, { Component } from "react";
+import ReactGA from "react-ga";
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
+import About from "./Components/About";
+import Resume from "./Components/Resume";
+import Contact from "./Components/Contact";
+import Testimonials from "./Components/Testimonials";
+import Portfolio from "./Components/Portfolio";
 
 class App extends Component {
-
   constructor(props) {
     super(props);
     this.state = {
-      foo: 'bar',
+      foo: "bar",
       resumeData: {}
     };
 
-    ReactGA.initialize('UA-122881407-1');
+    ReactGA.initialize("UA-122881407-1");
     ReactGA.pageview(window.location.pathname);
-
   }
 
   getResumeData() {
-    $.ajax({
-      url: '/resumeData.json',
-      dataType: 'json',
-      cache: false,
-      success: function (data) {
-        this.setState({ resumeData: data });
-      }.bind(this),
-      error: function (xhr, status, err) {
-        console.log(err);
-        alert(err);
-      }
-    });
+    fetch("/resumeData.json")
+      .then(response => response.json())
+      .then(result => this.setState({ resumeData: result }))
+      .catch(error => console.log("error:", error));
   }
 
   componentDidMount() {
